55ec4a2b15c4ff0411531826e7f236ed923fcf5e
Zhenya <zh.basalay@gmail.com>
2022-09-14 20:34:57 +0300

    Task 1, equals, hashCode ....

---
+++ src/Lesson13092022/Person.java
@@ -0,0 +1,63 @@
+package Lesson13092022;
+
+import java.util.Objects;
+
+
+public class Person {
+    public static void main(String[] args) {
+
+        Person person_1 = new Person("Alex", "WilsoN");
+        Person person_2 = new Person("Alex", "Wilson");
+
+        System.out.println(person_1.equals(person_2));
+
+    }
+
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof Person))
+            return false;
+        Person person = (Person) o;
+        return Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
+
